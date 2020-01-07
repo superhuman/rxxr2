e37eafc21aa83795df1ea50e8f4e82676dfd7121
Brian Zindler <zindlerb@gmail.com>
2020-01-06 17:21:36 -0800

    Update design

--- regex.rip/src/app.tsx
+++ regex.rip/src/app.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import { CheckResult, CheckResultVulnerable, check } from './check.tsx'
+import RegexLogo from './components/regex_logo'
+import SuperhumanLogo from './components/superhuman_logo'
+import RegexInput from './components/regex_input'
 import 'style/index.scss'
 import TextareaAutosize from 'react-textarea-autosize'
 import classNames from 'classnames'
@@ -10,16 +13,17 @@ export default function App () {
   let [loading, setLoading] = React.useState(false)
   let [error, setError] = React.useState<Error | null>(null)
   let [result, setResult] = React.useState<CheckResult | null>(null)
+  let [isFocused, setFocus] = React.useState(false)
 
   React.useEffect(() => {
     (async () => {
       let regexWas = regex
       try {
-        console.log(regex)
+        setError(null)
+        setResult(null)
+
         if ((regex === '' || regex === '//') && result == null) {
           setLoading(false)
-          setError(null)
-          setResult(null)
           setRegex('')
           return
         }
@@ -60,40 +64,74 @@ export default function App () {
     setInput(e.target.value)
   }
 
-  const className = classNames('input', {
-    'empty': regex === '' || input !== regex,
-    'loading': loading,
-    'vulnerable': !loading && regex === input && result && result.result === 'vulnerable',
-    'ok': !loading && regex === input && result && result.input && result.result === 'ok',
-    'error': !loading && regex === input && error
-  })
+  let inputStatus
+
+  if (regex === input && error) {
+    inputStatus = 'error'
+  } else if (regex === input && result && result.result === 'vulnerable') {
+    inputStatus = 'vulnerable'
+  } else if (regex === input && result && result.input && result.result === 'ok') {
+    inputStatus = 'ok'
+  } else if (isFocused) {
+    inputStatus = 'focused'
+  }
 
   return <>
-    <h1>TODO HEADER</h1>
-    <p>Enter a regular expression to test whether it is vulnerable to <a href='https://medium.com/p/f8983bb76afc/edit'>ReDOS</a>.</p>
-    <div className={className}>
-      <TextareaAutosize value={input} onChange={handleChange} placeholder='/(a|b|ab)*c/' onKeyDown={handleKeyDown} />
-      <div className='progress'/>
-      { error ? (
-        <>
-         <h3>ERROR</h3>
-         <code>{error.message}</code>
-         <p>Sorry, something went wrong. Please check your regex and try again.</p>
-        </>
-      ) : null}
-      { result && result.input && result.result === 'ok' ? (
-        <>
-          <h3>GREAT</h3>
-          <code>{result.input.startsWith('/') ? result.input : '/' + result.input + '/'}</code>
-          <p>This regular expression is not vulnerable to exponential backtracking.</p>
-        </>
-      ) : null}
-      { result && result.result === 'vulnerable' ? (
-        renderVulnerable(result)
-      ) : null}
-      <hr/>
-      <p><small><a href='/'>regex.rip</a> is a project from <a href='https://superhuman.com/jobs'>Superhuman Labs</a>. If you're interested in helping us solve the problems required to build the fastest email client in the world, ping <a href='mailto:conrad.irwin@superhuman.com?subject=re:+regex.rip'>conrad.irwin@superhuman.com</a>.</small></p>
-      <p><small>N.B. This service is provided as-is. Please join in on <a href='https://github.com/superhuman/rxxr2'>GitHub</a> to help make improvements.</small></p>
+    <div className="body">
+      <div className="text-block">
+        <RegexLogo />
+        <p className="instruction-text">Enter a regular expression to test whether it is vulnerable to <a href='https://medium.com/p/f8983bb76afc'>ReDOS</a>.</p>
+      </div>
+      <div className='input-block'>
+        <RegexInput
+          loading={loading}
+          status={inputStatus}
+          value={input}
+          onChange={handleChange}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          placeholder='/(a|b|ab)*c/'
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+      <div className="text-block result-container">
+        { error ? (
+            <>
+              <p><code>{error.message}</code></p>
+              <p>Sorry, something went wrong. Please check your regex and try again.</p>
+            </>
+        ) : null}
+        { result && result.input && result.result === 'ok' ? (
+            <>
+              <p><code>{result.input.startsWith('/') ? result.input : '/' + result.input + '/'}</code></p>
+              <p>This regular expression is not vulnerable to exponential backtracking.</p>
+            </>
+        ) : null}
+        { result && result.result === 'vulnerable' ? (
+            renderVulnerable(result)
+        ) : null}
+      </div>
+    </div>
+    <div className="footer">
+      <div className="input-block"><hr/></div>
+      <div className="text-block flex flex-wrap justify-between">
+        <SuperhumanLogo/>
+        <div className="footer-text">
+          <p className="mt0">
+            <small>
+              <a href='/'>regex.rip</a> is a project from <a href='https://superhuman.com/jobs'>Superhuman Labs</a>.
+              If you&#39;re interested in helping us solve the problems required to build the fastest email client in the world,
+              ping <a href='mailto:conrad.irwin@superhuman.com?subject=re:+regex.rip'>conrad.irwin@superhuman.com</a>.
+            </small>
+          </p>
+          <p className="small-line-break">
+            <small>
+              N.B. This service is provided as-is.
+              Please join in on <a href='https://github.com/superhuman/rxxr2'>GitHub</a> to help make improvements.
+            </small>
+          </p>
+        </div>
+      </div>
     </div>
   </>
 }
@@ -109,19 +147,21 @@ function renderVulnerable (result: CheckResultVulnerable) {
   kleeneIndex += 1
 
   return (<>
-    <h3>VULNERABLE</h3>
-    <code>
-      {regex.slice(0, kleeneIndex)}
-      <span className='vulnerable'>{regex.slice(kleeneIndex, kleeneIndex + kleeneLength)}</span>
-      {regex.slice(kleeneIndex + kleeneLength)}
-    </code>
-    <p>This regular expression is vulnerable to exponential backtracking.</p>
     <p>
-      When matching strings of the form <code>^{result.prefix}({result.pumpable})*{result.suffix}$</code>, the time taken for matching to fail will double with each additional <code>{result.pumpable}</code>.
-    </p>
-    <p>For example: copy and paste this into a javascript console:</p>
-      <code>
-        ({regex}).test({JSON.stringify(result.prefix)} + {JSON.stringify(result.pumpable)}.repeat(25) + {JSON.stringify(result.suffix)})
+      <code className="vulnerable-regex">
+        {regex.slice(0, kleeneIndex)}
+        <span className='vulnerable'>{regex.slice(kleeneIndex, kleeneIndex + kleeneLength)}</span>
+        {regex.slice(kleeneIndex + kleeneLength)}
       </code>
+    </p>
+    <p>
+      This regular expression is vulnerable to exponential backtracking.
+      When matching strings of the form <code>^{result.prefix}({result.pumpable})*{result.suffix}$</code>,
+          the time taken for matching to fail will double with each additional <code>{result.pumpable}</code>.
+    </p>
+    <p className="line-break">For example: copy and paste this into a javascript console:</p>
+    <code>
+      ({regex}).test({JSON.stringify(result.prefix)} + {JSON.stringify(result.pumpable)}.repeat(25) + {JSON.stringify(result.suffix)})
+    </code>
   </>)
 }
